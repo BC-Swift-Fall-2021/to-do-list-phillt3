99da51ed96921cc62594ba174ab40b9c5e208f2b
phillt3 <tracypb@bc.edu>
2021-09-25 21:16:26 -0400

    5.7 todo list saving data complete!

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Phillip  Tracy on 9/24/21.
+//
+
+import Foundation
+
+struct ToDoItem : Codable {
+    var name: String
+    var date: Date
+    var notes: String
+}
